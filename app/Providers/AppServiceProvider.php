911264d1e0f28689634dac4bdbb5965effcee27e
kyubicode <sigitxlsantoso@gmail.com>
2026-09-15 23:35:25 +0700

    force https scheme for assets and routes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -32,6 +32,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        URL::forceScheme('https');
+        
         View::composer('frontend.*', function ($view) {
             $navigations = [];
             
